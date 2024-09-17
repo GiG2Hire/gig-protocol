b2efd9c3b986a8e5033f56e590a750362c43aea1
Toheeb Alade <aladetoheeb5@gmail.com>
2024-09-17 17:30:56 +0100

    Fix: authenticate github from backend

--- src/app/job-marketplace/page.tsx
+++ src/app/job-marketplace/page.tsx
@@ -9,18 +9,6 @@ import githubVerification from "../actions/verify-github";
 
 const JobMarketplace: NextPage = () => {
 
-  useEffect(() => {
-    const githubVerificationProcess = async () => {
-      const url = window.location.href;
-      const code = url.split("?code=")[1];
-      if (code && await githubVerification(code)) {
-        console.log("GitHub verification done!!!");
-        window.location.href = "/freelancer-dashboard";
-      }
-    };
-
-    githubVerificationProcess();
-  }, []);
   return (
     <div className={styles.jobMarketplace}>
       <main className={styles.pageContent}>
