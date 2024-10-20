f6dd5743eccd77e63413e3af6809d5c19e4dfdc4
mgroovyank <mayanksamariya99@gmail.com>
2024-10-20 20:46:12 +0530

    secure-thirdweb-secret

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -1,7 +1,7 @@
 import { createThirdwebClient } from "thirdweb";
 
 const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!;
-const secretKey = process.env.NEXT_PUBLIC_THIRDWEB_SECRET_KEY!;
+const secretKey = process.env.THIRDWEB_SECRET_KEY!;
 
 const options: any = secretKey
   ? { secretKey }
