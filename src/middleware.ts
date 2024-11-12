cc98336e0d1f2e07b52e00f0fb8a04c339ba13f3
mgroovyank <mayanksamariya99@gmail.com>
2024-11-12 22:06:39 +0530

    update-main

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { getPayload, isLoggedIn } from "./app/actions/login";
 import { CLIENT, FREELANCER } from "./constants/appConstants";
-import { decodeJWT } from "thirdweb/utils";
+// import { decodeJWT } from "thirdweb/utils";
+import { jwtDecode } from "jwt-decode";
 
 /**
  * This function can be marked `async` if using `await` inside
@@ -14,14 +15,23 @@ export async function middleware(request: NextRequest) {
 
   const pathname = request.nextUrl.pathname;
   const isSignInPage = pathname == "/sign-in" ? true : false;
+  const isChatWindow = pathname.startsWith("/chat");
 
   // Get user id and role from jwt
   const jwtObject = request.cookies.get("jwt");
+  console.log(request.cookies);
+  console.log("jwt decode:", jwtObject);
+
   if (jwtObject == undefined) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  const jwt = decodeJWT(request.cookies.get("jwt")?.value as string);
-  const ctx: JWTContext = jwt.payload.ctx as JWTContext;
+
+  // const jwt = decodeJWT(request.cookies.get("jwt")?.value as string);
+  const jwt = jwtDecode(request.cookies.get("jwt")?.value as string);
+  console.log("jwt decode:", jwt);
+  // const ctx: JWTContext = jwt.payload.ctx as JWTContext;
+  const ctx = jwt.ctx;
+
   const userId = ctx.userId;
   const role = ctx.role;
 
@@ -36,9 +46,41 @@ export async function middleware(request: NextRequest) {
     }
   }
 
+  if (isChatWindow) {
+    console.log("Inside chat window");
+    console.log(request.nextUrl);
+    const cloneUrl = request.nextUrl.clone();
+    cloneUrl.searchParams.set("userId", userId);
+    request.nextUrl.searchParams.set("userId", userId);
+    const searchParams = request.nextUrl;
+    console.log("use search params ", userId);
+    if (role != FREELANCER && role != CLIENT) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    const id = request.nextUrl.pathname.split("/")[2];
+    console.log("------------------", id);
+    if (role == FREELANCER) {
+      if (userId == id.split("-")[1]) {
+        console.log("Trying to access valid chat!");
+      }
+    } else if (role == CLIENT) {
+      console.log("Accessing chat as client!");
+      if (userId == id.split("-")[0]) {
+        console.log("Trying to access valid chat!");
+      }
+    }
+    console.log("return rewritten response");
+    console.log(request.nextUrl.searchParams);
+    console.log(cloneUrl);
+    // return NextResponse.rewrite(
+    //   new URL("http://localhost:3000/chat/2-1-1?userId=2")
+    // );
+    console.log(request.nextUrl.searchParams.get("userId"));
+    return NextResponse.rewrite(cloneUrl);
+  }
+
   return NextResponse.next();
   // const isFreelancerDashboard = pathname.startsWith("/freelancer-dashboard");
-  // const isChatWindow = pathname.startsWith("/chat");
   // const isAuth = await isLoggedIn();
 
   // if (!isAuth) {
@@ -59,38 +101,7 @@ export async function middleware(request: NextRequest) {
   //     return NextResponse.redirect(new URL("/", request.url));
   //   }
   // }
-  // if (isChatWindow) {
-  //   console.log("Inside chat window");
-  //   console.log(request.nextUrl);
-  //   const cloneUrl = request.nextUrl.clone();
-  //   cloneUrl.searchParams.set("userId", payload.ctx.userId);
-  //   request.nextUrl.searchParams.set("userId", payload.ctx.userId);
-  //   const searchParams = request.nextUrl;
-  //   console.log("use search params ", payload.ctx.userId);
-  //   if (payload.ctx.role != FREELANCER && payload.ctx.role != CLIENT) {
-  //     return NextResponse.redirect(new URL("/", request.url));
-  //   }
-  //   const id = request.nextUrl.pathname.split("/")[2];
-  //   console.log("------------------", id);
-  //   if (payload.ctx.role == FREELANCER) {
-  //     if (payload.ctx.userId == id.split("-")[1]) {
-  //       console.log("Trying to access valid chat!");
-  //     }
-  //   } else if (payload.ctx.role == CLIENT) {
-  //     console.log("Accessing chat as client!");
-  //     if (payload.ctx.userId == id.split("-")[0]) {
-  //       console.log("Trying to access valid chat!");
-  //     }
-  //   }
-  //   console.log("return rewritten response");
-  //   console.log(request.nextUrl.searchParams);
-  //   console.log(cloneUrl);
-  //   // return NextResponse.rewrite(
-  //   //   new URL("http://localhost:3000/chat/2-1-1?userId=2")
-  //   // );
-  //   console.log(request.nextUrl.searchParams.get("userId"));
-  //   return NextResponse.rewrite(cloneUrl);
-  // }
+
   // // Continue processing if no condition is met
   // return NextResponse.next();
   //   return NextResponse.redirect(new URL("/freelancer-dashboard", request.url));
@@ -98,6 +109,6 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/sign-in"],
+  matcher: ["/sign-in", "/chat/:path*"],
   // matcher: ["/freelancer-dashboard/:path*", "/chat/:path*"],
 };
