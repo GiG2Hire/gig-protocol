d38b808bf0b4bf5151306ae75ddb383d7673abfe
mgroovyank <mayanksamariya99@gmail.com>
2024-11-15 17:59:55 +0530

    fix-active-link-issue

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   images: {
     remotePatterns: [
       {
